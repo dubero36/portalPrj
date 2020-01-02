b39dffb9d0f8997fb75718de12af5e9285fa0775
dubero36 <dubero36@gmail.com>
2020-01-02 22:16:07 +0900

    local commit #3

--- src/main/java/com/dubero/portal/test1/User1.java
+++ src/main/java/com/dubero/portal/test1/User1.java
@@ -5,5 +5,7 @@ public class User1 {
 		System.out.println("User1.print()");
 		
 		System.out.println("local.commit");
+		
+		System.out.println("local.commit2");
 	}
 }
--- src/main/java/com/dubero/portal/test1/User2.java
+++ src/main/java/com/dubero/portal/test1/User2.java
@@ -4,8 +4,8 @@ public class User2 {
 	public void print() {
 		System.out.println("User2.print()");
 		
-		System.out.println("local.commit");
+		System.out.println("local2.commit");
 		
-		System.out.println("remote.commit");
+		System.out.println("remote2.commit");
 	}
 }
