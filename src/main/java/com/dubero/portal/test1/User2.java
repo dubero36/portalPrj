ba9e5dab18aac1c7a8f4c70be87f4b0a5d77591a
dubero36 <dubero36@gmail.com>
2020-01-02 22:29:44 +0900

    fix5

--- src/main/java/com/dubero/portal/test1/User2.java
+++ src/main/java/com/dubero/portal/test1/User2.java
@@ -4,13 +4,6 @@ public class User2 {
 	public void print() {
 		System.out.println("User2.print()");
 		
-<<<<<<< HEAD
 		System.out.println("local.commit #4");
-=======
-		System.out.println("local2.commit");
-		System.out.println("local3.commit");
-		
-		System.out.println("remote.commit #4");
->>>>>>> branch 'master' of https://github.com/dubero36/portalPrj.git
 	}
 }
