38b39bcae12a2a9fd690368f2d3f80ff1a319271
dubero36 <56665939+dubero36@users.noreply.github.com>
2020-01-02 22:15:15 +0900

    remote commit #3

--- src/main/java/com/dubero/portal/test1/User2.java
+++ src/main/java/com/dubero/portal/test1/User2.java
@@ -4,8 +4,8 @@ public class User2 {
 	public void print() {
 		System.out.println("User2.print()");
 		
-		System.out.println("local.commit");
+		System.out.println("local3.commit");
 		
-		System.out.println("remote.commit");
+		System.out.println("remote3.commit");
 	}
 }
